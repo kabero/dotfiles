a3291bf63e00a56cc0a93ec6f92595d3993f2115
kabe <q4bero@gmail.com>
2022-08-01 00:56:13 +0900

    Modify the config for nvim-transitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,15 @@ require 'keymaps'
 require 'commands'
 require 'color'
 
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "all",
+    highlight = {
+        enable = true,
+        disable = {
+        }
+    }
+}
+
 -- Run PackerCompile when plugins.lua is edited
 vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
 
