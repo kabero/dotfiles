cda011317fdc4941030e2cdb5853b0fbb8b8fad5
kabero <83853529+kabero@users.noreply.github.com>
2023-03-16 14:32:43 +0900

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,19 +1,38 @@
 -- disable some standard plugins
+-----------------------------
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- load configs
+-----------------------------
 require "options"
 require "color"
 require "keymap"
 require "plugins"
 
 -- configs of plugins
+-----------------------------
+local opts = { noremap = true, silent = true }
 require("nvim-tree").setup()
 require("gitsigns").setup()
 
-local opts = { noremap = true, silent = true }
+-- nvim-tree
 vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
+
+-- fzf
+vim.api.nvim_set_keymap("n", "<leader>f", ":Files<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>g", ":GFiles<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>G", ":GFiles?<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>b", ":Buffers<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>h", ":History<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>r", ":Rg<CR>", opts)
+
+-- whichkey
+vim.api.nvim_set_keymap("n", "<leader>w", ":WhichKey<CR>", opts)
+
+
+-- other configs
+-----------------------------
 
 -- open a quickfix window when executing a command whose name contains "grep"
 vim.cmd([[autocmd QuickFixCmdPost *grep* cwindow]])
