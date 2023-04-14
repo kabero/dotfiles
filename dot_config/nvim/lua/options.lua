b644853c99d2b8ba4294a516f59d18b9cdfb7b4b
kabe <83853529+kabero@users.noreply.github.com>
2023-04-14 15:22:23 +0900

    update

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -4,6 +4,8 @@ local cmd = vim.cmd
 -- General
 cmd("language C")
 cmd("set encoding=utf-8")
+cmd("let g:python3_host_prog = expand('/Users/ko_abe/.pyenv/versions/nvim3/bin/python')")
+cmd("let g:ruby_host_prog = expand('~/.rbenv/versions/3.0.6/bin/ruby')")
 opt.clipboard:append { 'unnamedplus' }
 opt.mouse = 'a'
 opt.fileformats = 'unix', 'mac', 'dos'
