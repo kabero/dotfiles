eee1b315c185419b102e1c426daaf0d6e90aef5f
kabe <83853529+kabero@users.noreply.github.com>
2023-05-08 06:43:16 +0900

    update

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -490,8 +490,8 @@ require('lazy').setup({
         'glidenote/memolist.vim',
         config = function()
             vim.g.memolist_path = '~/.memolist/memo'
-            vim.g.memolist_memo_suffix = 'md'
             vim.g.memolist_fzf = 1
+            vim.g.memolist_memo_suffix = 'md'
             vim.g.memolist_template_dir_path = '~/.memolist/memotemplates'
         end
     },
