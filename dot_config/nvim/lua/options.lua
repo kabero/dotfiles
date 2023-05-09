c2bc987708cf2bd7f4eee16fb9d8ce5e6a810246
kabe <83853529+kabero@users.noreply.github.com>
2023-05-09 20:44:44 +0900

    update

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -2,8 +2,10 @@
 vim.cmd("language C")
 vim.cmd("set encoding=utf-8")
 -- vim.cmd("let g:python3_host_prog = expand('/Users/ko_abe/.pyenv/versions/nvim3/bin/python')")
-vim.cmd("let g:python3_host_prog = system('type asdf &>/dev/null && echo -n $(asdf which python) || echo -n $(which python)')")
-vim.cmd("let g:ruby_host_prog = system('type asdf &>/dev/null && echo -n $(asdf which ruby) || echo -n $(which ruby)')")
+-- vim.cmd("let g:python3_host_prog = system('type asdf &>/dev/null && echo -n $(asdf which python) || echo -n $(which python)')")
+-- vim.cmd("let g:ruby_host_prog = system('type asdf &>/dev/null && echo -n $(asdf which ruby) || echo -n $(which ruby)')")
+vim.cmd("let g:python3_host_prog = expand('~/.asdf/installs/python/3.9.1/bin/python')")
+vim.cmd("let g:ruby_host_prog = expand('~/.asdf/installs/ruby/3.1.2/bin/ruby')")
 
 vim.opt.clipboard:append { 'unnamedplus' }
 vim.opt.mouse = 'a'
