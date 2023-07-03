de5fbab298d884644fa7fa58a1db9bdb765b728c
q4bero <83853529+kabero@users.noreply.github.com>
2023-07-04 02:10:22 +0900

    update

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,11 +1,9 @@
 -- General
 vim.cmd("language C")
 vim.cmd("set encoding=utf-8")
--- vim.cmd("let g:python3_host_prog = expand('/Users/ko_abe/.pyenv/versions/nvim3/bin/python')")
--- vim.cmd("let g:python3_host_prog = system('type asdf &>/dev/null && echo -n $(asdf which python) || echo -n $(which python)')")
--- vim.cmd("let g:ruby_host_prog = system('type asdf &>/dev/null && echo -n $(asdf which ruby) || echo -n $(which ruby)')")
 vim.cmd("let g:python3_host_prog = expand('~/.asdf/installs/python/3.9.1/bin/python')")
 vim.cmd("let g:ruby_host_prog = expand('~/.asdf/installs/ruby/3.1.2/bin/ruby')")
+vim.cmd("colorscheme gruvbox")
 
 vim.opt.ambiwidth = "single"
 vim.opt.clipboard:append { 'unnamedplus' }
