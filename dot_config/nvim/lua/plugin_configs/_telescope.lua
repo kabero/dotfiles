5c227b035ac8aa69cf2ca6036efbcf29ad62743f
kabero <83853529+kabero@users.noreply.github.com>
2023-09-26 09:41:43 +0900

    update

--- dot_config/nvim/lua/plugin_configs/_telescope.lua
+++ dot_config/nvim/lua/plugin_configs/_telescope.lua
@@ -4,11 +4,31 @@ telescope.setup {
     defaults = {
         mappings = {
             i = {
-                ['<C-u>'] = actions.results_scrolling_up,
-                ["<C-d>"] = actions.results_scrolling_down,
+                -- scroll results
+                -- ["<C-u>"] = actions.results_scrolling_up,
+                -- ["<C-d>"] = actions.results_scrolling_down,
+
+                -- scroll preview
+                ["<C-u>"] = actions.preview_scrolling_up,
+                ["<C-d>"] = actions.preview_scrolling_down,
+
+                -- open in split window
+                ["<C-l>"] = actions.select_vertical,
+                ["<C-j>"] = actions.select_horizontal,
+
+                -- emacs-like mappings
+                ["<C-b>"] = { "<left>", type = "command" },
+                ["<C-f>"] = { "<right>", type = "command" },
+                ["<C-a>"] = { "<c-o>0", type = "command" },
+                ["<C-e>"] = { "<c-o>$", type = "command" },
+                ["<C-k>"] = { "<c-o>D", type = "command" },
+                ["<C-h>"] = { "<BS>", type = "command" },
+                ["<M-BS>"] = { "<c-o>db<BS>", type = "command" },
+
                 -- use telescope only in insert mode
                 ['<C-]>'] = actions.close,
                 ['<ESC>'] = actions.close,
+                ['<C-c>'] = actions.close,
             },
             n = {
                 ["q"] = require("telescope.actions").close
@@ -45,9 +65,16 @@ telescope.setup {
 }
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>fh', '<cmd>lua require("telescope.builtin").oldfiles()<CR>', opts)
-vim.keymap.set('n', '<leader>fj', '<cmd>lua require("telescope.builtin").find_files()<CR>', opts)
-vim.keymap.set('n', '<leader>fk', '<cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
-vim.keymap.set('n', '<leader>fl', '<cmd>lua require("telescope.builtin").grep_string()<CR>', opts)
-vim.keymap.set('n', '<leader>fi', '<cmd>lua require("telescope.builtin").git_bcommits()<CR>', opts)
-vim.keymap.set('n', '<leader>fo', '<cmd>lua require("telescope.builtin").buffers()<CR>', opts)
+-- Finder
+vim.keymap.set('n', '<leader>fh', ':Telescope oldfiles theme=ivy<CR>', opts)
+vim.keymap.set('n', '<leader>fj', ':Telescope find_files theme=ivy<CR>', opts)
+vim.keymap.set('n', '<leader>fk', ':Telescope live_grep theme=ivy<CR>', opts)
+vim.keymap.set('n', '<leader>fl', ':Telescope grep_string theme=ivy<CR>', opts)
+vim.keymap.set('n', '<leader>fi', ':Telescope jumplist theme=ivy show_line=false<CR>', opts)
+vim.keymap.set('n', '<leader>fo', ':Telescope diagnostics theme=ivy<CR>', opts)
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers theme=ivy<CR>', opts)
+
+-- Git
+vim.keymap.set('n', '<leader>dj', ':Telescope git_commits theme=ivy<CR>', opts)
+vim.keymap.set('n', '<leader>dk', ':Telescope git_bcommits theme=ivy<CR>', opts)
+vim.keymap.set('n', '<leader>dl', ':Telescope git_status theme=ivy<CR>', opts)
