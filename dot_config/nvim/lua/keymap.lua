fbe9f4ac4af7b98626c8e276b703500d843ab7f1
kabero <83853529+kabero@users.noreply.github.com>
2023-10-30 23:33:13 +0900

    update

--- dot_config/nvim/lua/keymap.lua
+++ dot_config/nvim/lua/keymap.lua
@@ -29,6 +29,8 @@ vim.g.mapleader = " "
 ------------------------------------
 vim.api.nvim_set_keymap("n", "j", "gj", opts)
 vim.api.nvim_set_keymap("n", "k", "gk", opts)
+vim.api.nvim_set_keymap("n", "gj", "j", opts)
+vim.api.nvim_set_keymap("n", "gk", "k", opts)
 vim.api.nvim_set_keymap("n", "]b", ":<c-u>bnext<CR>", opts)
 vim.api.nvim_set_keymap("n", "[b", ":<c-u>bprev<CR>", opts)
 vim.api.nvim_set_keymap("n", "]q", ":<c-u>cnext<CR>", opts)
@@ -57,6 +59,7 @@ vim.api.nvim_set_keymap("c", "<C-d>", "<Del>", { noremap = true })
 vim.api.nvim_set_keymap("c", "<C-h>", "<BS>", { noremap = true })
 -- FIXME: The behavior when the cursor is a the beginning of a line
 vim.api.nvim_set_keymap("c", "<C-k>", "<C-f>D<C-c><C-c>:<Up>", { noremap = true })
+vim.api.nvim_set_keymap("c", "<M-BS>", "<c-w>", { noremap = true })
 
 -- Terminal
 ------------------------------------
