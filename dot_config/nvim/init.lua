6e2e42babbc30329c2d9227d617970fc950c68a9
kabero <83853529+kabero@users.noreply.github.com>
2024-03-25 14:50:48 +0900

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -71,17 +71,5 @@ vim.cmd [[
     autocmd TermOpen * setlocal nonumber
 ]]
 
--- Highlighting
-vim.cmd [[
-    augroup myHighlighting
-        autocmd!
-        autocmd Colorscheme * highlight EndOfBuffer ctermbg=none guibg=none
-        autocmd Colorscheme * highlight Folded ctermbg=none guibg=none
-        autocmd Colorscheme * highlight LineNr ctermbg=none guibg=none
-        autocmd Colorscheme * highlight NonText ctermbg=none guibg=none
-        autocmd Colorscheme * highlight Normal ctermbg=none guibg=none
-        autocmd Colorscheme * highlight NormalNC ctermbg=none guibg=none
-        autocmd Colorscheme * highlight TabLineFill ctermbg=none guibg=none
-    augroup END
-]]
-vim.cmd([[colorscheme everforest]])
+-- Color settings should be at the bottom.
+require "colors"
