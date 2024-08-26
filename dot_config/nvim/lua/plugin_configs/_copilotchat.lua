b489a2b8d1ca62d7a24c49320ea752ed39354ea1
kabero <83853529+kabero@users.noreply.github.com>
2024-08-27 03:42:29 +0900

    update

---
+++ dot_config/nvim/lua/plugin_configs/_copilotchat.lua
@@ -0,0 +1,61 @@
+local chat = require("CopilotChat")
+chat.setup({
+    show_help = "yes",
+    prompts = {
+        Explain = {
+            prompt = "/COPILOT_EXPLAIN 上記のコードを日本語で説明してください",
+            mapping = '<leader>ce',
+            description = "コードの説明",
+        },
+        Review = {
+            prompt = '/COPILOT_REVIEW 選択したコードをレビューしてください。レビューコメントは日本語でお願いします。',
+            mapping = '<leader>cr',
+            description = "コードのレビュー",
+        },
+        Fix = {
+            prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードを表示してください。説明は日本語でお願いします。",
+            mapping = '<leader>cf',
+            description = "コードの修正",
+        },
+        Optimize = {
+            prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。説明は日本語でお願いします。",
+            mapping = '<leader>co',
+            description = "コードの最適化",
+        },
+        Docs = {
+            prompt = "/COPILOT_GENERATE 選択したコードに関するドキュメントコメントを日本語で生成してください。",
+            mapping = '<leader>cd',
+            description = "コードのドキュメント作成",
+        },
+        Tests = {
+            prompt = "/COPILOT_TESTS 選択したコードの詳細なユニットテストを書いてください。説明は日本語でお願いします。",
+            mapping = '<leader>ct',
+            description = "コードのテストコード作成",
+        },
+        FixDiagnostic = {
+            prompt = 'コードの診断結果に従って問題を修正してください。修正内容の説明は日本語でお願いします。',
+            mapping = '<leader>cd',
+            description = "コードの静的解析結果に基づいた修正",
+            selection = require('CopilotChat.select').diagnostics,
+        },
+        Commit = {
+            prompt =
+            'commitize の規則に従って、変更に対するコミットメッセージを記述してください。 タイトルは最大50文字で、メッセージは72文字で折り返されるようにしてください。 メッセージ全体を gitcommit 言語のコードブロックでラップしてください。メッセージは日本語でお願いします。',
+            mapping = '<leader>cc',
+            description = "コミットメッセージの作成",
+            selection = require('CopilotChat.select').gitdiff,
+        },
+        CommitStaged = {
+            prompt =
+            'commitize の規則に従って、ステージ済みの変更に対するコミットメッセージを記述してください。 タイトルは最大50文字で、メッセージは72文字で折り返されるようにしてください。 メッセージ全体を gitcommit 言語のコードブロックでラップしてください。メッセージは日本語でお願いします。',
+            mapping = '<leader>cs',
+            description = "ステージ済みのコミットメッセージの作成",
+            selection = function(source)
+                return require('CopilotChat.select').gitdiff(source, true)
+            end,
+        },
+    },
+    window = {
+        layout = 'float'
+    },
+})
