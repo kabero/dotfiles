e7496d519bc4ccb2ef0759ac86cb2eae3947947f
q4bero <83853529+kabero@users.noreply.github.com>
2024-09-11 06:58:01 +0900

    update

--- dot_config/nvim/lua/colors.lua
+++ dot_config/nvim/lua/colors.lua
@@ -10,9 +10,15 @@ vim.cmd [[
         autocmd Colorscheme * highlight NormalNC ctermbg=none guibg=none
         autocmd Colorscheme * highlight TabLineFill ctermbg=none guibg=none
         autocmd Colorscheme * set cursorline
-        autocmd Colorscheme * highlight CursorLine ctermbg=none guibg=#2a2a2a
+        autocmd Colorscheme * highlight CursorLine ctermbg=none guibg=#212121
         autocmd Colorscheme * highlight CopilotSuggestion guifg=#bb00cc ctermfg=blue
         autocmd Colorscheme * highlight SignColumn guibg=NONE
+        autocmd Colorscheme * highlight Search guibg=#7a6d94 guifg=#000000
+        autocmd Colorscheme * highlight IncSearch guibg=#5d20d6 guifg=#FFFFFF
+
+        autocmd Colorscheme * highlight TabLineSel guibg=NONE guifg=#aaaaaa
+        autocmd Colorscheme * highlight TabLine guibg=#3a3a3a guifg=#d0d0d0
+        autocmd Colorscheme * highlight TabLineFill guibg=#1c1c1c guifg=#d0d0d0
     augroup END
 ]]
 
