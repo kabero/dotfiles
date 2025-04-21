8f7708805bd4b4540009ba1d959c96be59979bae
q4bero <83853529+kabero@users.noreply.github.com>
2025-04-22 05:48:29 +0900

    update

--- dot_config/nvim/lua/plugin_configs/_lualine.lua
+++ dot_config/nvim/lua/plugin_configs/_lualine.lua
@@ -109,7 +109,7 @@ ins_left {
 ins_left {
     'filename',
     filestatus = true,
-    path = 3,
+    path = 1,
     cond = conditions.buffer_not_empty,
     color = { fg = colors.white, gui = 'bold' },
 }
