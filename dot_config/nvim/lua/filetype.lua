3d31d83cc347ff2877db2114f220343247314d01
q4bero <83853529+kabero@users.noreply.github.com>
2025-04-23 06:25:12 +0900

    update

--- dot_config/nvim/lua/filetype.lua
+++ dot_config/nvim/lua/filetype.lua
@@ -26,3 +26,16 @@ vim.api.nvim_command('autocmd BufRead,BufNewFile .env.* setlocal filetype=sh')
 
 -- yaml
 vim.api.nvim_command('autocmd FileType yaml setlocal foldmethod=indent | normal zR')
+
+-- tmpl
+vim.filetype.add({
+    extension = {
+        tmpl = function(path, bufnr)
+            local original_ext = path:match("%.([^.]+)%.tmpl$")
+            if original_ext then
+                return vim.filetype.match({ filename = path:gsub("%.tmpl$", "") })
+            end
+            return "text"
+        end,
+    },
+})
