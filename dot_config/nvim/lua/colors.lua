5345fe3830a9b3fa999c79caac4817fceb3c4547
kabero <83853529+kabero@users.noreply.github.com>
2025-11-12 03:08:39 +0900

    update

--- dot_config/nvim/lua/colors.lua
+++ dot_config/nvim/lua/colors.lua
@@ -1,40 +1,45 @@
 -- To use vim's colors, comment out the folloing lines.
-vim.cmd [[
-    augroup myHighlighting
-        autocmd!
-        autocmd Colorscheme * highlight EndOfBuffer ctermbg=none guibg=none
-        autocmd Colorscheme * highlight Folded ctermbg=none guibg=none
-        autocmd Colorscheme * highlight LineNr ctermbg=none guibg=none guifg=#707070
-        autocmd Colorscheme * highlight CursorLineNr ctermbg=none guibg=none guifg=#b14a4a
-        autocmd Colorscheme * highlight NonText ctermbg=none guibg=none
-        autocmd Colorscheme * highlight Normal ctermbg=none guibg=none
-        autocmd Colorscheme * highlight NormalNC ctermbg=none guibg=none
-        autocmd Colorscheme * set cursorline
-        autocmd Colorscheme * highlight CopilotSuggestion guifg=#bb00cc ctermfg=blue
-        autocmd Colorscheme * highlight SignColumn guibg=NONE
-        autocmd Colorscheme * highlight CursorLine ctermbg=none guibg=#2f2f2f
+local function setup_highlights()
+    local highlights = {
+        { 'EndOfBuffer', { bg = 'none' } },
+        { 'Folded', { bg = 'none' } },
+        { 'LineNr', { bg = 'none', fg = '#707070' } },
+        { 'CursorLineNr', { bg = 'none', fg = '#b14a4a' } },
+        { 'NonText', { bg = 'none' } },
+        { 'Normal', { bg = 'none' } },
+        { 'NormalNC', { bg = 'none' } },
+        { 'CopilotSuggestion', { fg = '#bb00cc' } },
+        { 'SignColumn', { bg = 'NONE' } },
+        { 'CursorLine', { bg = '#2f2f2f' } },
+        -- Comment
+        { 'Comment', { bg = 'none', fg = '#6f6f6f' } },
+        -- Search
+        { 'Search', { bg = '#7a6d94', fg = '#000000' } },
+        { 'IncSearch', { bg = '#5d20d6', fg = '#FFFFFF' } },
+        -- Tabline
+        { 'TabLineSel', { bg = 'NONE', fg = '#b14a4a' } },
+        { 'TabLine', { bg = 'NONE', fg = '#aaaaaa' } },
+        { 'TabLineFill', { bg = 'NONE', fg = '#d0d0d0' } },
+        -- Telescope
+        { 'TelescopeMatching', { bg = 'none', fg = '#5f9ea0' } },
+        -- Winbar
+        { 'WinBar', { bg = 'NONE', fg = '#d3c6aa' } },
+        { 'WinBarNC', { bg = 'NONE', fg = '#888888' } },
+    }
 
-        " Comment
-        autocmd Colorscheme * highlight Comment ctermbg=none guibg=none guifg=#6f6f6f
+    for _, hl in ipairs(highlights) do
+        vim.api.nvim_set_hl(0, hl[1], hl[2])
+    end
 
-        " Search
-        autocmd Colorscheme * highlight Search guibg=#7a6d94 guifg=#000000
-        autocmd Colorscheme * highlight IncSearch guibg=#5d20d6 guifg=#FFFFFF
-        
-        " Tabline
-        autocmd Colorscheme * highlight TabLineSel guibg=NONE guifg=#b14a4a
-        autocmd Colorscheme * highlight TabLine guibg=NONE guifg=#aaaaaa
-        autocmd Colorscheme * highlight TabLineFill guibg=NONE guifg=#d0d0d0
+    vim.cmd([[highlight clear TelescopeSelection]])
+    vim.cmd([[highlight link TelescopeSelection CursorLine]])
+    vim.opt.cursorline = true
+end
 
-        " Telescope
-        autocmd Colorscheme * highlight TelescopeMatching guibg=none guifg=#5f9ea0
-        autocmd Colorscheme * highlight clear TelescopeSelection
-        autocmd Colorscheme * highlight link TelescopeSelection CursorLine
-        
-        " Winbar
-        autocmd Colorscheme * highlight WinBar guibg=NONE guifg=#d3c6aa
-        autocmd Colorscheme * highlight WinBarNC guibg=NONE guifg=#888888
-    augroup END
-]]
+vim.api.nvim_create_augroup('myHighlighting', { clear = true })
+vim.api.nvim_create_autocmd('Colorscheme', {
+    group = 'myHighlighting',
+    callback = setup_highlights,
+})
 
 vim.cmd([[colorscheme everforest]])
